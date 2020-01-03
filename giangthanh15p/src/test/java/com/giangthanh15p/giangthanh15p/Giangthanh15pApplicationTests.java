c944ea8134b639bff664d6bd5b6dad147e351faf
ThanhNam99 <43337709+ThanhNam99@users.noreply.github.com>
2020-01-03 17:48:10 +0700

    OK

---
+++ giangthanh15p/src/test/java/com/giangthanh15p/giangthanh15p/Giangthanh15pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giangthanh15p.giangthanh15p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Giangthanh15pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
